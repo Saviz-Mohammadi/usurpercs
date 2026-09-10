1344ab3e19fb06f713413d0453146c846eb4c5bb
Saviz Mohammadi <savizmohammadi1379@gmail.com>
2026-09-09 23:06:13 -0700

    In this commit, I added the official logo for the platform. It is hand-crafted using a free and open-source software called Inkscape.
    NOTES:
    0- The project will compile successfully using Eleventy. However, there is still no actual content and you may not observe any output when running the project.
    1- The logo itself is in SVG format and is licensed under the 'CC BY 4.0' license. In addition, the metadata is attached to the file.
    2- I understand that the choice may not seem ideal, but at the moment, it is the best graphical solution I can provide given my current skill set.
    3- The logo will be used directly within the content of the pages in the future (currently planned for use in the header and footer).
    4- To use the logo as a favicon, I used a website called 'RealFaviconGenerator'. Here are a couple of notes regarding this:
       -- This is the most sophisticated website I could find for generating favicons at the moment.
       -- The website suggests putting the resource files directly in the root for maximum cross-platform support, which is why I did not place them in a dedicated location.
       -- The website also provided me with the necessary HTML code and commands to make the files work. I have tested this and confirmed that it works in a separate, contained environment.
    5- I have also added some pass-through statements to the configuration file named 'eleventy.config.js' to ensure that all favicon files are copied to the destination.
    
     Changes to be committed:
    	modified:   eleventy.config.js
                        Added some pass-through statements to ensure that all favicon files are copied to the destination.
    
            Added the following graphical files:
    	added:   src/apple-touch-icon.png
    	added:   src/assets/site/graphics/usurpercs_logo.svg
    	added:   src/favicon-96x96.png
    	added:   src/favicon.ico
    	added:   src/favicon.svg
    	added:   src/site.webmanifest
    	added:   src/web-app-manifest-192x192.png
    	added:   src/web-app-manifest-512x512.png

--- eleventy.config.js
+++ eleventy.config.js
@@ -9,6 +9,15 @@ export default async function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/css");
   eleventyConfig.addPassthroughCopy("src/js");
 
+  // Favicon
+  eleventyConfig.addPassthroughCopy("src/apple-touch-icon.png");
+  eleventyConfig.addPassthroughCopy("src/favicon.ico");
+  eleventyConfig.addPassthroughCopy("src/favicon.svg");
+  eleventyConfig.addPassthroughCopy("src/favicon-96x96.png");
+  eleventyConfig.addPassthroughCopy("src/site.webmanifest");
+  eleventyConfig.addPassthroughCopy("src/web-app-manifest-192x192.png");
+  eleventyConfig.addPassthroughCopy("src/web-app-manifest-512x512.png");
+
   // Filters
   // ---------------------------------------------------------------------------
   eleventyConfig.addFilter("sortByOrder", function sortByOrder(items) {
