0a8097de93710e76a7018e66d0d7c23f95df2c21
Saviz Mohammadi <savizmohammadi1379@gmail.com>
2026-09-12 22:23:01 -0700

    In this commit, I added the first set of testing content to the project.
    NOTES:
    0- The project will compile successfully using Eleventy.
    1- It only contains a testing 'Hello World!' message as the content for now, as I want to plan the structure a bit more.
    
     Changes to be committed:
    	added:   src/_data/metadata.js
                     This file contains crucial metadata that will be used throughout the project.
    
    	added:   src/_includes/layouts/base.njk
                     This file serves as the base template for all pages and other content.
    
    	added:   src/css/style.css
                     The CSS file that contains most of the style definitions and is used throughout the project.
    
    	added:   src/index.njk
                     The file that represents the home page of the website. Contains minimal content for now.

---
+++ src/_data/metadata.js
@@ -0,0 +1,6 @@
+export default {
+  authorName: 'Saviz Mohammadi',
+  websiteName: 'UsurperCS',
+  websiteDescription:
+    'A free online tutorial site covering a variety of computer-related topics and technologies.',
+};
