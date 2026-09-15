2ddcad9168121a625ca31cb6799bf23bff5d3569
Saviz Mohammadi <savizmohammadi1379@gmail.com>
2026-09-14 18:26:11 -0700

    In this commit, I added a global data file for storing the main navigation links.
    NOTES:
    0- The project will compile successfully using Eleventy.
    1- I am aware that I could have used a solution such as the 'Navigation Plugin'
       offered by Eleventy, but I am a simple developer and like to stick to custom
       and simple solutions.
       Here is the link: 'https://www.11ty.dev/docs/plugins/navigation/'
    
      Changes to be committed:
        added: src/_data/navigation.js
               This file contains the main and important navigation links (not all)
               for the website. This is because I want only certain, important
               links to be used in critical parts such as the 'header' and
               'footer', so they aren't cluttered with too many links.

---
+++ src/_data/navigation.js
@@ -0,0 +1,12 @@
+// NOTE (SAVIZ):
+// 0- This is a data structure that contains the main navigation links.
+// 1- I am aware that I could have used a solution such as the
+//    'Navigation Plugin' offered by Eleventy, but I am a simple developer and
+//    like to stick to custom and simple solutions.
+// link: 'https://www.11ty.dev/docs/plugins/navigation/'
+export default [
+  { title: 'Home', url: '/' },
+  { title: 'Tutorials', url: '/tutorials/' },
+  { title: 'About', url: '/about/' },
+  { title: 'Contact', url: '/contact/' },
+];
