3b1a7c147c1e65a0a94c2463f4b4a6c99c992ca8
Saviz Mohammadi <savizmohammadi1379@gmail.com>
2026-09-17 20:59:56 -0700

    In this commit, I added the additional data structure files needed in the '_data' folder to accommodate the required links for the future footer.
    NOTES:
    0- The project will compile successfully using Eleventy.
    
      Changes to be committed:
        added: src/_data/legal.js
               This file contains the links to important legal pages for the
               website.
    
        added: src/_data/social.js
               This file contains the links to important social media platforms for
               the website.

---
+++ src/_data/legal.js
@@ -0,0 +1,11 @@
+// NOTE (SAVIZ):
+// 0- This is a data structure that contains the links to various legal pages.
+// 1- Please note that this list is subject to change at any time in the future.
+export default [
+  { title: 'Privacy Policy', url: '' },
+  { title: 'Terms of Service', url: '' },
+  { title: 'Disclaimer', url: '' },
+  { title: 'License Agreement', url: '' },
+  { title: 'Cookie Policy', url: '' },
+  { title: 'Privacy Settings', url: '' },
+];
---
+++ src/_data/social.js
@@ -0,0 +1,9 @@
+// NOTE (SAVIZ):
+// 0- This is a data structure that contains the links to various social media
+//    platforms.
+// 1- Please note that this list is subject to change at any time in the future.
+export default [
+  { title: 'YouTube', url: '' },
+  { title: 'GitHub', url: '' },
+  { title: 'LinkedIn', url: '' },
+];
